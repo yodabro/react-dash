ba9e8d9c9db35833db5b58ef20133e9e58d0b9be
yodabro _ <dmitriybodan@gmail.com>
2022-05-29 12:16:34 -0700

    routes fixed

--- src/App.js
+++ src/App.js
@@ -68,17 +68,25 @@ const App = () => {
 							{/* dashboard */}
 							<Route path="/" element={<Ecommerce />} />
 							<Route path="/ecommerce" element={<Ecommerce />} />
-							{/* pages */} 
+							{/* pages */}
 							<Route path="/orders" element={<Orders />} />
 							<Route path="/employees" element={<Employees />} />
 							<Route path="/customers" element={<Customers />} />
-							{/* apps */} 
+							{/* apps */}
 							<Route path="/kanban" element={<Kanban />} />
 							<Route path="/editor" element={<Editor />} />
 							<Route path="/calendar" element={<Calendar />} />
 							<Route path="/color-picker" element={<ColorPicker />} />
-							{/* charts */} 
-
+							{/* charts */}
+							<Route path="/line" element={<Line />} />
+							<Route path="/area" element={<Area />} />
+							<Route path="/bar" element={<Bar />} />
+							<Route path="/pie" element={<Pie />} />
+							<Route path="/financial" element={<Financial />} />
+							<Route path="/color-mapping" element={<ColorMapping />} />
+							<Route path="/pyramid" element={<Pyramid />} />
+							<Route path="/stacked" element={<Stacked />} />
+						</Routes>
 					</div>
 				</div>
 			</BrowserRouter>
