66a030ee696b99090fb18af2a16655abff568468
yodabro _ <dmitriybodan@gmail.com>
2022-05-29 12:16:10 -0700

    small fixes

--- src/App.js
+++ src/App.js
@@ -68,25 +68,17 @@ const App = () => {
 							{/* dashboard */}
 							<Route path="/" element={<Ecommerce />} />
 							<Route path="/ecommerce" element={<Ecommerce />} />
-							{/* pages */}
+							{/* pages */} 
 							<Route path="/orders" element={<Orders />} />
 							<Route path="/employees" element={<Employees />} />
 							<Route path="/customers" element={<Customers />} />
-							{/* apps */}
+							{/* apps */} 
 							<Route path="/kanban" element={<Kanban />} />
 							<Route path="/editor" element={<Editor />} />
 							<Route path="/calendar" element={<Calendar />} />
 							<Route path="/color-picker" element={<ColorPicker />} />
-							{/* charts */}
-							<Route path="/line" element={<Line />} />
-							<Route path="/area" element={<Area />} />
-							<Route path="/bar" element={<Bar />} />
-							<Route path="/pie" element={<Pie />} />
-							<Route path="/financial" element={<Financial />} />
-							<Route path="/color-mapping" element={<ColorMapping />} />
-							<Route path="/pyramid" element={<Pyramid />} />
-							<Route path="/stacked" element={<Stacked />} />
-						</Routes>
+							{/* charts */} 
+
 					</div>
 				</div>
 			</BrowserRouter>
