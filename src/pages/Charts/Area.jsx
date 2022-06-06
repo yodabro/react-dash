0b698d8a7eafff2383ac6dffaa22efc5cbb3f1d1
yodabro _ <dmitriybodan@gmail.com>
2022-06-06 12:33:45 -0700

    charts work continues

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -23,8 +23,6 @@ const Area = () => {
 		<div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
 			<Header category="Area" title="Inflation Rate in Percentage" />
 			<ChartComponent
-				id="area-chart"
-				height="420px"
 				primaryXAxis={areaPrimaryXAxis}
 				primaryYAxis={areaPrimaryYAxis}
 				chartArea={{ border: { width: 0 } }}
